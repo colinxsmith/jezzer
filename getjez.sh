73148c6073b8434f4b310448b5c8fcfe64a840b6
Colin Smith <colinxsmith@gmail.com>
2017-11-29 22:49:30 +0000

    It works again, tidy up later

--- getjez.sh
+++ getjez.sh
@@ -14,29 +14,29 @@ echo $filename
 #url=`sed -n "/\.mp3'/p" $filename | awk -F\' '{print $2}'`
 url="$filename"
 echo $url
-music=`echo $url | sed "s|.*/jazz|jazz|"`
-target=$(echo $music | awk -F_ '{print $2}'|awk -F- '{print "Jez_"$3"-"$2"-"$1".aac"}')
+music=$url #`echo $url | filenames=`sed -n "/mp3/p" index.html.1 |sed "s/\\\\\\\\//g"| sed "s/^.*http/http/;s/\".*//"``
+target=$(echo $url | awk -F_ '{print "Jez_"$2"-"$3"-20"$4}')
 echo $music
 echo $target
 echo ${music%.mp3}.aac
-if ! [ -f $target ]
+if ! [ -f ${target%.mp3}.aac ]
 then
-echo get $music
+echo get $url
 #gfile=$(sed -n "/\.mp3'/p" $filename | awk -F\' '{print $2}')
 wget $url
 music=`echo $filename|sed "s/^.*pod/pod/"`
 echo $music
-avconv -i $music -codec: copy $target
+avconv -i $music -codec: copy ${target%.mp3}.aac
 fi
 rm $music
 done
 rm index.html*
-ls -l $target
-echo $target|sed "s/.*_//;s/\..*//"|awk -F- '{ print "date --date="$2"/"$1"/"$3" +%s" }'|bash
+ls -l ${target%.mp3}.aac
+echo ${target%.mp3}.aac|sed "s/.*_//;s/\..*//"|awk -F- '{ print "date --date="$2"/"$1"/"$3" +%s" }'|bash
 ls -l --time-style=+%s $target | awk '{ print $6 }'
 for jez in Jez*
 do
-filetime=$(echo $jez|sed "s/.*_//;s/\..*//"|awk -F- '{ print "date --date="$2"/"$1"/"$3" +%s" }'|bash)
+filetime=$(echo $jez|sed "s/.*_//;s/\..*//"|awk -F- '{ print "date --date="$2"/"$1"/"$3" +%s" }'|bash)e
 filetime=$(ls -l --time-style=+%s $jez | awk '{ print $6 }')
 now=$(date +%s)
 fileage=$(echo $now-$filetime | awk -F- '{ a=($1-$2)/3600/24;b=(a-int(a))*24;c=(b-int(b))*60;d=(c-int(c))*60;printf("\x1b[1;1;31m%d days %d hours %d minutes %d seconds\x1b[0;m", int(a),b,c,d);}')
