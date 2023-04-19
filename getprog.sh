e72c8f9029c207bcec39b5d8574c929c091449b9
Colin <colinxsmith@gmail.com>
2023-04-19 23:20:19 +0100

    better kkk

--- getprog.sh
+++ getprog.sh
@@ -28,8 +28,10 @@ echo "Time now $(date +\%T)"
 ./get_iplayer --pvr --type=tv -o /home/colin/Videos
 
 cd /home/colin/Music
+KKK=kkk`date +%s`.aac
+echo $KKK
 
-for f in *.m4a;do if ! [ -f ${f%.m4a}.mp3 ]; then rate=`mediainfo $f| awk '/Bit rate/{print $4}'|sed -n "/[0-9]/p"`;ffmpeg -i "$f" -codec: copy kkk.aac;ffmpeg -i kkk.aac -ab ${rate}k "${f%.m4a}.mp3";rm kkk.aac;tt=$(ls -l --time-style +%s "${f%.m4a}.mp3"|awk '{print $6}');echo $tt;art=$(echo "${f%.m4a}.mp3" |awk -F "-" '{print $1}'|sed "s/_/ /g");tit=$(echo "${f%.m4a}.mp3" |awk -F "-" '{print $2}'|sed "s/_/ /g;s/\.mp3//g");id3v2 -A "BBC Recording" -a "$art" -t "$tit" "${f%.m4a}.mp3";id3v2 -l "${f%.m4a}.mp3";touch -m --date="@$tt" "${f%.m4a}.mp3";fi; done
+for f in *.m4a;do if ! [ -f ${f%.m4a}.mp3 ]; then rate=`mediainfo $f| awk '/Bit rate/{print $4}'|sed -n "/[0-9]/p"`;ffmpeg -i "$f" -codec: copy $KKK;ffmpeg -i $KKK -ab ${rate}k "${f%.m4a}.mp3";rm $KKK;tt=$(ls -l --time-style +%s "${f%.m4a}.mp3"|awk '{print $6}');echo $tt;art=$(echo "${f%.m4a}.mp3" |awk -F "-" '{print $1}'|sed "s/_/ /g");tit=$(echo "${f%.m4a}.mp3" |awk -F "-" '{print $2}'|sed "s/_/ /g;s/\.mp3//g");id3v2 -A "BBC Recording" -a "$art" -t "$tit" "${f%.m4a}.mp3";id3v2 -l "${f%.m4a}.mp3";touch -m --date="@$tt" "${f%.m4a}.mp3";fi; done
 
 for i in *.mp3;do if ! [ -f ${i%.mp3}.m4a ]; then echo $i; rm $i;fi; done
 
