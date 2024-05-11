c0cae97afc3c5d628427c929606588e67a9b4375
Colin Smith <colinxsmith@gmail.com>
2024-05-11 20:59:22 +0100

    Change limit to 40 days

--- timelapse.sh
+++ timelapse.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+limit=40
 now=$(date +%s)
 for i in *original.m4a *original.mp4
 do
@@ -6,7 +7,7 @@ filetime=$(ls -l --time-style=+%s $i | awk '{print $6}')
 #echo $i,$filetime,$now,$filetime
 daytest=$(echo $now,$filetime | awk -F, '{print int(($1-$2)/60/60/24)}')
 #echo $i $daytest
-if [ $daytest -gt 30 ] 
+if [ $daytest -gt $limit ] 
 then
 echo deleting $i $daytest
 rm $i
