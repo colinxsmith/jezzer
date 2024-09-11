3146da25ed022562fc07099465b360026b2d3fc6
Colin Smith <colinxsmith@gmail.com>
2024-09-11 21:10:27 +0100

    .

--- timelapse.sh
+++ timelapse.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 limit=40
 now=$(date +%s)
-for i in *original.m4a *original.mp4
+for i in *original.m4a *original.mp4 Brown*.m4a
 do
 filetime=$(ls -l --time-style=+%s $i | awk '{print $6}')
 #echo $i,$filetime,$now,$filetime
